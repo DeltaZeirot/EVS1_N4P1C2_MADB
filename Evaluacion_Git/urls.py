2dd8956b28cc405660f1bcaa6ac56835bf5fdf38
Delta <mdiazbarriga02@gmail.com>
2023-09-25 15:09:38 -0300

    app2 terminada con vistas

--- Evaluacion_Git/urls.py
+++ Evaluacion_Git/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app2 import views as v2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vista1r2/', v2.vista1r2),
+    path('vista2r2/', v2.vista2r2),
 ]
