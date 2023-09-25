4369080b6e7c6e99130c26507a6897837413e81c
Delta <mdiazbarriga02@gmail.com>
2023-09-25 15:12:29 -0300

    arreglos en colores rama2

--- app2/views.py
+++ app2/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 def vista1r2(request):
     html= """
     
-    <h1 style="color: blue">Bienvenida rama2</h1>
+    <h1 style="color: orange">Pagina bienvenida a la rama 1</h1>
     
     """
     return HttpResponse(html)
@@ -15,7 +15,7 @@ def vista2r2(request):
     
     <h1 style="font-family:Monocraft">Segunda vista de la rama 2</h1>
     
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quia doloribus, quaerat natus ipsa itaque. Praesentium sequi, eius explicabo illo tempora distinctio repudiandae soluta? Nihil quae odit provident molestias assumenda.</p>
+    <p style="font-family:Monocraft;color:orange">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quia doloribus, quaerat natus ipsa itaque. Praesentium sequi, eius explicabo illo tempora distinctio repudiandae soluta? Nihil quae odit provident molestias assumenda.</p>
     
     """
     return HttpResponse(html)
